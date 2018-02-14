75c9f4dfbeb682f6f9c18c5a0cfe191cb7515486
yuwtsri <35005989+yuwtsri@users.noreply.github.com>
2018-02-14 11:43:39 -0800

    Add files via upload

--- Plot/Gene_expression_plot_each.py
+++ Plot/Gene_expression_plot_each.py
@@ -14,6 +14,7 @@ from pandas import ExcelWriter
 from textwrap import wrap
 
 cols = ["gene", "subgroup", "total_number", "favorable_5_year", "favorable_10_year", "logrank_P"]
+limit =10
 def filedir(y, key):
 	root = "X:\\Su Lab\\TCGA\\Data\\Matrix\\"
 	filedir = root + y + "\\" + y + "-" + key + "-matrix.csv"
@@ -77,15 +78,18 @@ def kmplot(df_high, df_low, ax):
 	return(p_value, hm5, hm10, lm5, lm10)
 	
 def divide_into_two(gene_matrix, index_matrix, t):
-	specific_gene_matrix = gene_matrix[['bcr_sample_barcode', t]]
+	col_id = [col for col in gene_matrix if col.startswith(t)]
+	col_id.append('bcr_sample_barcode')
+	#print(col_id)
+	specific_gene_matrix = gene_matrix[col_id]
 	key = ['bcr_sample_barcode']
 	in1 = index_matrix.set_index(key).index
 	in2 = specific_gene_matrix.set_index(key).index
 	total_number = len(index_matrix)
 	specific_gene_matrix = specific_gene_matrix[in2.isin(in1)]
-	gene_cutoff = specific_gene_matrix[t].quantile(0.5)
-	high_index = specific_gene_matrix[specific_gene_matrix[t] > gene_cutoff]
-	low_index = specific_gene_matrix[specific_gene_matrix[t] < gene_cutoff]
+	gene_cutoff = specific_gene_matrix.ix[:,0].quantile(0.5)
+	high_index = specific_gene_matrix[specific_gene_matrix.ix[:,0] > gene_cutoff]
+	low_index = specific_gene_matrix[specific_gene_matrix.ix[:,0] < gene_cutoff]
 	ihigh = high_index.set_index(key).index
 	ilow = low_index.set_index(key).index
 	df_high = index_matrix[in1.isin(ihigh)]
@@ -101,14 +105,17 @@ def survival_compare(h, l):
 	else:
 		return("NA")
 
-def save_fig(summary_index, total_matrix, gene_matrix, t, ARS, plottitle, filetitle, tumor_name):
+def save_fig(y, summary_index, total_matrix, gene_matrix, t, ARS, plottitle, filetitle, tumor_name):
 
 
 	df_high, df_low, total_number = divide_into_two(gene_matrix, total_matrix, t)
 	
 	fig = plt.figure()
 	fig.suptitle("\n".join(wrap(('Expression of ' + str(ARS) + ' in ' + tumor_name))), fontsize = 13)
-	fig.subplots_adjust(top = 0.85)
+	if (y == "TCGA-CESC") or (y == "TCGA-DLBC"):
+		fig.subplots_adjust(top = 0.80)
+	else:
+		fig.subplots_adjust(top = 0.85)
 	ax = plt.subplot(111)
 	ax.set_title("\n".join(wrap(plottitle)), fontsize = 11)
 	plt.ylim(0, 1)
@@ -129,31 +136,69 @@ def extract_filter(y, t, ARS, total_matrix, summary_index, tumor_name):
 	os.mkdir('X:\\Su Lab\\TCGA\\Data\\Plot\\' + y + "\\" + ARS)
 	
 	matrix_normal = total_matrix[total_matrix['sample_type'] == "Solid Tissue Normal"]
-	summary_index = save_fig(summary_index, matrix_normal, gene_matrix, t, ARS, "Solid Tissue Normal", "normal_tissue", tumor_name)
+	if len(matrix_normal) > limit:
+		summary_index = save_fig(y, summary_index, matrix_normal, gene_matrix, t, ARS, "Solid Tissue Normal", "normal_tissue", tumor_name)
 
 	matrix_meta = total_matrix[total_matrix['sample_type'] == "Metastatic"]
-	summary_index = save_fig(summary_index, matrix_meta, gene_matrix, t, ARS, "Metastatic", "metastatic", tumor_name)	
+	if len(matrix_meta) > limit:
+		summary_index = save_fig(y, summary_index, matrix_meta, gene_matrix, t, ARS, "Metastatic", "metastatic", tumor_name)	
 	
 	# primary tumor
 	matrix_primary = total_matrix[total_matrix['sample_type'] == "Primary Tumor"]
-	summary_index = save_fig(summary_index, matrix_primary, gene_matrix, t, ARS, "Primary Tumor", "primary_tumor", tumor_name)
+	if y != "TCGA-LAML":
+		if len(matrix_primary) > limit:
+			summary_index = save_fig(y, summary_index, matrix_primary, gene_matrix, t, ARS, "Primary Tumor", "primary_tumor", tumor_name)
+	else:
+		matrix_primary = total_matrix[total_matrix['sample_type'] == "Primary Blood Derived Cancer - Peripheral Blood"]
+		if len(matrix_primary) > limit:
+			summary_index = save_fig(y, summary_index, matrix_primary, gene_matrix, t, ARS, "Primary Blood Derived Cancer", "primary_blood_cancer", tumor_name)
+
 	# ============================================================
 	matrix_male = matrix_primary[matrix_primary['gender'] == "male"]
-	summary_index = save_fig(summary_index, matrix_male, gene_matrix, t, ARS, "Primary Tumor: Male", "gender_male", tumor_name)
+	if len(matrix_male) > limit:
+		if y == "TCGA-LAML":
+			summary_index = save_fig(y, summary_index, matrix_male, gene_matrix, t, ARS, "Primary Blood Derived Cancer: Male", "primary_blood_cancer_gender_male", tumor_name)
+		else:
+			summary_index = save_fig(y, summary_index, matrix_male, gene_matrix, t, ARS, "Primary Tumor: Male", "primary_tumor_gender_male", tumor_name)
 	matrix_female = matrix_primary[matrix_primary['gender'] == "female"]
-	summary_index = save_fig(summary_index, matrix_female, gene_matrix, t, ARS, "Primary Tumor: Female", "gender_female", tumor_name)
+	if len(matrix_female) > limit:
+		if y == "TCGA-LAML":
+			summary_index = save_fig(y, summary_index, matrix_female, gene_matrix, t, ARS, "Primary Blood Derived Cancer: Female", "primary_blood_cancer_gender_female", tumor_name)
+		else:
+			summary_index = save_fig(y, summary_index, matrix_female, gene_matrix, t, ARS, "Primary Tumor: Female", "primary_tumor_gender_female", tumor_name)
+
 	#=====================================
 	matrix_i = matrix_primary[matrix_primary['tumor_stage'].str.startswith('stage i') == True]
 	matrix_i = matrix_i[matrix_i['tumor_stage'].str.startswith('stage ii') == False]
 	matrix_i = matrix_i[matrix_i['tumor_stage'].str.startswith('stage iv') == False]
-	summary_index = save_fig(summary_index, matrix_i, gene_matrix, t, ARS, "Primary Tumor: Stage I", "primary_tumor_stage_i", tumor_name)
+
+	if len(matrix_i) > limit:
+		if y == "TCGA-LAML":
+			summary_index = save_fig(y, summary_index, matrix_i, gene_matrix, t, ARS, "Primary Blood Derived Cancer: Stage I", "primary_blood_cancer_stage_i", tumor_name)
+		else:
+			summary_index = save_fig(y, summary_index, matrix_i, gene_matrix, t, ARS, "Primary Tumor: Stage I", "primary_tumor_stage_i", tumor_name)
+
 	matrix_ii = matrix_primary[matrix_primary['tumor_stage'].str.startswith('stage ii') == True]
 	matrix_ii = matrix_ii[matrix_ii['tumor_stage'].str.startswith('stage iii') == False]
-	summary_index = save_fig(summary_index, matrix_ii, gene_matrix, t, ARS, "Primary Tumor: Stage II", "primary_tumor_stage_ii", tumor_name)	
+	if len(matrix_ii) > limit:
+		if y == "TCGA-LAML":
+			summary_index = save_fig(y, summary_index, matrix_ii, gene_matrix, t, ARS, "Primary Blood Derived Cancer: Stage II", "primary_blood_cancer_stage_ii", tumor_name)
+		else:
+			summary_index = save_fig(y, summary_index, matrix_ii, gene_matrix, t, ARS, "Primary Tumor: Stage II", "primary_tumor_stage_ii", tumor_name)	
+
 	matrix_iii = matrix_primary[matrix_primary['tumor_stage'].str.startswith('stage iii') == True]
-	summary_index = save_fig(summary_index, matrix_iii, gene_matrix, t, ARS, "Primary Tumor: Stage III", "primary_tumor_stage_iii", tumor_name)
+	if len(matrix_iii) > limit:
+		if y == "TCGA-LAML": 
+			summary_index = save_fig(y, summary_index, matrix_iii, gene_matrix, t, ARS, "Primary Blood Derived Cancer: Stage III", "primary_blood_cancer_stage_iii", tumor_name)
+		else:
+			summary_index = save_fig(y, summary_index, matrix_iii, gene_matrix, t, ARS, "Primary Tumor: Stage III", "primary_tumor_stage_iii", tumor_name)
+
 	matrix_iv = matrix_primary[matrix_primary['tumor_stage'].str.startswith('stage iv') == True]
-	summary_index = save_fig(summary_index, matrix_iv, gene_matrix, t, ARS, "Primary Tumor: Stage IV", "primary_tumor_stage_iv", tumor_name)
+	if len(matrix_iv) > limit:
+		if y == "TCGA-LAML":
+			summary_index = save_fig(y, summary_index, matrix_iv, gene_matrix, t, ARS, "Primary Blood Derived Cancer: Stage IV", "primary_blood_cancer_stage_iv", tumor_name)
+		else:
+			summary_index = save_fig(y, summary_index, matrix_iv, gene_matrix, t, ARS, "Primary Tumor: Stage IV", "primary_tumor_stage_iv", tumor_name)
 
 	# =========from count file
 	with open("X:\\Su Lab\\TCGA\\Data\\Matrix\\" + y + "\\" + y + "-clinical-count.json") as json_file:
@@ -161,9 +206,16 @@ def extract_filter(y, t, ARS, total_matrix, summary_index, tumor_name):
 		for ele in d:
 			category = d[ele]
 			for item in category:
-				if (category[item] > 10) and (item != "NotApplicable") and (len(category) < 10):
-					matrix_ele = matrix_primary[matrix_primary[ele] == item]
-					summary_index = save_fig(summary_index, matrix_ele, gene_matrix, t, ARS, ("Primary Tumor: " + ele.replace("_", " ").title() + " " + item), (ele + "_" + (item.replace(" ", "_").replace("/", "_")).lower()), tumor_name)
+					if (category[item] > limit) and (item != "NotApplicable") and (len(category) < 10):
+						try:
+							matrix_ele = matrix_primary[matrix_primary[ele] == item]
+							if y == "TCGA-LAML":
+								summary_index = save_fig(y, summary_index, matrix_ele, gene_matrix, t, ARS, ("Primary Blood Derived Cancer: " + ele.replace("_", " ").title() + " " + item.title()), "primary_blood_cancer_"+(ele.replace(" ", "_").replace("/", "_").replace("<", "less_than").replace("=", "").replace(">", "more_than").replace(":","_") + "_" + (item.replace(" ", "_").replace("/", "_").replace("<", "less_than").replace("=", "").replace(">", "more_than").replace(":","_")).lower()), tumor_name)
+							else:
+								summary_index = save_fig(y, summary_index, matrix_ele, gene_matrix, t, ARS, ("Primary Tumor: " + ele.replace("_", " ").title() + " " + item.title()), "primary_tumor_" + (ele.replace(" ", "_").replace("/", "_").replace("<", "less_than").replace("=", "").replace(">", "more_than").replace(":","_") + "_" + (item.replace(" ", "_").replace("/", "_").replace("<", "less_than").replace("=", "").replace(">", "more_than").replace(":","_")).lower()), tumor_name)
+
+						except TypeError:
+							continue
 	return(summary_index)
 
 
@@ -190,14 +242,14 @@ if __name__ == '__main__':
 		
 		summary_index = pd.DataFrame()
 
-		with open("X:\\Su Lab\\TCGA\\Data\\Plot\\" + "genelist.txt") as f:
+		with open("X:\\Su Lab\\TCGA\\Data\\Plot\\" + "genelist-related.txt") as f:
 		    for line in f:
 		       (ARS, t) = line.split()
 		       summary_index = extract_filter(y, t, ARS, total_matrix, summary_index, tumor_name)
 		       print("================= " + ARS + " Done!!! ================")
 
 		summary_index.reset_index()
-		writer = pd.ExcelWriter("X:\\Su Lab\\TCGA\\Data\\Plot\\" + y + "\\" + y + "-summary.xlsx")
+		writer = pd.ExcelWriter("X:\\Su Lab\\TCGA\\Data\\Plot\\" + y + "\\" + y + "-related-gene-summary.xlsx")
 		summary_index.to_excel(writer, 'Sheet1')
 		writer.save()
 		print(y + " summary index written")
